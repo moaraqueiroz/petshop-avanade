031fc09a195efdf9d9d83cc9a2fb5f041ea265d9
moara queiroz <moaraqueiroz@gmail.com>
2021-03-25 10:53:35 -0300

    lista do petshop

---
+++ index.js
@@ -0,0 +1,50 @@
+const nomePetShop = "PETSHOP AVANADE"
+
+let pets = [
+{
+    nome: 'Apollo',
+    tipo: 'cachorro',
+    idade: 3,
+    raca: 'Pitbull',
+    peso: 35,
+    tutor: 'Moara',
+    contato: '(81) 98888-3106',
+    vacinado: true,
+    servicos: ['banho', 'corta unha']
+},
+
+{
+nome: 'Happy',
+tipo: 'cachorro',
+idade: 5,
+raca: 'Vira-Lata',
+peso: 20,
+tutor: 'Maria',
+contato: '(81) 98888-3106',
+vacinado: true,
+servicos: ['banho', 'corta unha']
+},
+
+{
+nome: 'Jubileu',
+tipo: 'gato',
+idade: 3,
+raca: 'Vira-Lata',
+peso: 5,
+tutor: 'Moara',
+contato: '(81) 98888-3106',
+vacinado: true,
+servicos: ['banho', 'corta unha']
+}
+];
+
+const listarPets = () => {
+    for (let i = 0; i < pets.length; i++){
+    console.log (pets[i].nome);
+        //função
+}
+    
+}
+listarPets(); //chamar função
+
+//console.log(pet);
